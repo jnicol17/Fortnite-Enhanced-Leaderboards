3e1d9850c2972247d2c1fc26d40a5489b9b10628
Jamie Nicol <jamie.nicol@mail.utoronto.ca>
2018-07-05 22:00:23 -0400

    Fixed printing Solo Stats and started Group Leaderboards
    Solo Stat printing is now much cleaner, cleaned it up to a 5 line code segment. Started populating group leaderboard dictionaries. Will work more on this this weekend.

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ solo_leaderboards = {}
 
 # These leaderboards will be populated with lists of dictionaries of player
 # stats that can then be sorted and displayed
-group_leaderboards = {"Leaderboards": {"Lifetime":[], "SoloStats":[], "DuoStats":[],
-    "SquadStats":[], "CurrSolo":[], "CurrDuo":[], "CurrSquad":[]}}
+group_leaderboards = {
+    "Lifetime":[],
+    "SoloStats":[],
+    "DuoStats":[],
+    "SquadStats":[],
+    "CurrSolo":[],
+    "CurrDuo":[],
+    "CurrSquad":[]
+    }
 
 # will store raw json data from fortnite tracker for all Players
 # format will be { player name : fortnitetracker data }
@@ -46,9 +53,9 @@ def add_remove_players():
         data = response.json()
         print(username)
 
-        #user = {username: data}
         populate_solo_leaderboards(username, data)
-        #name = input("Fortnite Name: ")
+        populate_group_leaderboards(username, data)
+
         sleep(1)
     menu()
 
@@ -58,28 +65,18 @@ def add_remove_players():
 # snipe_celly34's stats with one command
 def view_individual_stats():
     name = input("Enter Users name: ")
-    print_stats(raw_data[name])
+    print_solo_stats(name)
     menu()
 
 # populate the group leaderboard dictionary
 def populate_solo_leaderboards(username, data):
+
     print("populating leaderboards")
     raw_data[username] = data
+    solo_leaderboards[username] = {}
 
     lifetime_stats = data["lifeTimeStats"]
-
-    solo_lifetime_stats = data["stats"]["p2"]
-    duo_lifetime_stats = data["stats"]["p10"]
-    squad_lifetime_stats = data["stats"]["p9"]
-
-    solo_current_stats = data["stats"]["curr_p2"]
-    duo_current_stats = data["stats"]["curr_p10"]
-    squad_current_stats = data["stats"]["curr_p9"]
-
-    solo_leaderboards[username] = {"lifetime": {}, "solo": {}, "duo": {},
-        "squads": {}, "curr_solo": {}, "curr_duo": {}, "curr_squads": {}}
-
-    solo_leaderboards[username]["lifetime"] = {
+    solo_leaderboards[username]["LIFETIME STATS"] = {
         "Matches Played": lifetime_stats[7]["value"],
         "Wins": lifetime_stats[8]["value"],
         "Win %": lifetime_stats[9]["value"].strip("%"),
@@ -87,7 +84,8 @@ def populate_solo_leaderboards(username, data):
         "K/D": lifetime_stats[11]["value"]
         }
 
-    solo_leaderboards[username]["solo"] = {
+    solo_lifetime_stats = data["stats"]["p2"]
+    solo_leaderboards[username]["SOLO STATS"] = {
         "Matches Played": solo_lifetime_stats["matches"]["value"],
         "Wins": solo_lifetime_stats["top1"]["value"],
         "Win %": solo_lifetime_stats["winRatio"]["value"],
@@ -97,7 +95,8 @@ def populate_solo_leaderboards(username, data):
         "K/D": solo_lifetime_stats["kd"]["value"]
         }
 
-    solo_leaderboards[username]["duo"] = {
+    duo_lifetime_stats = data["stats"]["p10"]
+    solo_leaderboards[username]["DUO STATS"] = {
         "Matches Played": duo_lifetime_stats["matches"]["value"],
         "Wins": duo_lifetime_stats["top1"]["value"],
         "Win %": duo_lifetime_stats["winRatio"]["value"],
@@ -107,7 +106,8 @@ def populate_solo_leaderboards(username, data):
         "K/D": duo_lifetime_stats["kd"]["value"]
     }
 
-    solo_leaderboards[username]["squads"] = {
+    squad_lifetime_stats = data["stats"]["p9"]
+    solo_leaderboards[username]["SQUADS STATS"] = {
         "Matches Played": squad_lifetime_stats["matches"]["value"],
         "Wins": squad_lifetime_stats["top1"]["value"],
         "Win %": squad_lifetime_stats["winRatio"]["value"],
@@ -117,7 +117,8 @@ def populate_solo_leaderboards(username, data):
         "K/D": squad_lifetime_stats["kd"]["value"]
     }
 
-    solo_leaderboards[username]["curr_solo"] = {
+    solo_current_stats = data["stats"]["curr_p2"]
+    solo_leaderboards[username]["CURRENT SEASON SOLO STATS"] = {
         "Matches Played": solo_current_stats["matches"]["value"],
         "Wins": solo_current_stats["top1"]["value"],
         "Win %": solo_current_stats["winRatio"]["value"],
@@ -127,7 +128,8 @@ def populate_solo_leaderboards(username, data):
         "K/D": solo_current_stats["kd"]["value"]
     }
 
-    solo_leaderboards[username]["curr_duo"] = {
+    duo_current_stats = data["stats"]["curr_p10"]
+    solo_leaderboards[username]["CURRENT SEASON DUO STATS"] = {
         "Matches Played": duo_current_stats["matches"]["value"],
         "Wins": duo_current_stats["top1"]["value"],
         "Win %": duo_current_stats["winRatio"]["value"],
@@ -137,110 +139,32 @@ def populate_solo_leaderboards(username, data):
         "K/D": duo_current_stats["kd"]["value"]
     }
 
-    solo_leaderboards[username]["curr_squads"] = {
+    squad_current_stats = data["stats"]["curr_p9"]
+    solo_leaderboards[username]["CURRENT SEASON SQUAD STATS"] = {
         "Matches Played": squad_current_stats["matches"]["value"],
         "Wins": squad_current_stats["top1"]["value"],
         "Win %": squad_current_stats["winRatio"]["value"],
         "Top 3s": squad_current_stats["top3"]["value"],
         "Top 6s": squad_current_stats["top6"]["value"],
         "Kills": squad_current_stats["kills"]["value"],
-        "K/D": squad_current_stats["kd"]["value"]    
+        "K/D": squad_current_stats["kd"]["value"]
     }
 
-    print(solo_leaderboards[username])
+# print the solo stats for a given username
+def print_solo_stats(username):
+    for headers in solo_leaderboards[username]:
+        print(headers)
+        for keys, values in solo_leaderboards[username][headers].items():
+            print(keys + ": " + values)
 
-# print out select stats from the raw data dictionary for a given player,
-# will clean this up when I start storing all the data is cleaner dictionaries
-# instead of pulling from the raw JSON.
-def print_stats(data):
-    print("Name: " + data["epicUserHandle"])
-    print("Platform: " + data["platformNameLong"])
+def populate_group_leaderboards(username, data):
+    # add the new member
+    group_leaderboards["Lifetime"].append({"name" : username, "Wins" : data["lifeTimeStats"][8]["value"]})
+    print(group_leaderboards)
 
-    lifetime_stats = data["lifeTimeStats"]
-    print_lifetime_stats(lifetime_stats)
-
-    solo_lifetime_stats = data["stats"]["p2"]
-    duo_lifetime_stats = data["stats"]["p10"]
-    squad_lifetime_stats = data["stats"]["p9"]
-    print_solo_lifetime_stats(solo_lifetime_stats)
-    print_duo_lifetime_stats(duo_lifetime_stats)
-    print_squad_lifetime_stats(squad_lifetime_stats)
-
-    solo_current_stats = data["stats"]["curr_p2"]
-    duo_current_stats = data["stats"]["curr_p10"]
-    squad_current_stats = data["stats"]["curr_p9"]
-    print_solo_current_stats(solo_current_stats)
-    print_duo_current_stats(duo_current_stats)
-    print_squad_current_stats(squad_current_stats)
-
-def print_lifetime_stats(lifetime_stats):
-    print("\nLIFETIME TOTAL")
-    print(lifetime_stats[7]["key"] + ": " + lifetime_stats[7]["value"])
-    print(lifetime_stats[8]["key"] + ": " + lifetime_stats[8]["value"])
-    print(lifetime_stats[9]["key"] + ": " + lifetime_stats[9]["value"])
-    print(lifetime_stats[10]["key"] + ": " + lifetime_stats[10]["value"])
-    print(lifetime_stats[11]["key"] + ": " + lifetime_stats[11]["value"])
-
-def print_solo_lifetime_stats(solo_lifetime_stats):
-    print("\nLIFETIME SOLO STATS")
-    print("# Matches: " + solo_lifetime_stats["matches"]["value"])
-    print("# Wins: " + solo_lifetime_stats["top1"]["value"])
-    print("Win %: " + solo_lifetime_stats["winRatio"]["value"])
-    print("# Top 10s: " + solo_lifetime_stats["top10"]["value"])
-    print("# Top 25s: " + solo_lifetime_stats["top25"]["value"])
-    print("Kills: " + solo_lifetime_stats["kills"]["value"])
-    print("K/D: " + solo_lifetime_stats["kd"]["value"])
-    #print("# Top 25: " + test["stats"]["p2"]["top25"]["valueInt"])
-
-def print_duo_lifetime_stats(duo_lifetime_stats):
-    print("\nLIFETIME DUO STATS")
-    print("# Matches: " + duo_lifetime_stats["matches"]["value"])
-    print("# Wins: " + duo_lifetime_stats["top1"]["value"])
-    print("Win %: " + duo_lifetime_stats["winRatio"]["value"])
-    print("# Top 5s: " + duo_lifetime_stats["top5"]["value"])
-    print("# Top 12s: " + duo_lifetime_stats["top12"]["value"])
-    print("Kills: " + duo_lifetime_stats["kills"]["value"])
-    print("K/D: " + duo_lifetime_stats["kd"]["value"])
-
-def print_squad_lifetime_stats(squad_lifetime_stats):
-    print("\nLIFETIME SQUAD STATS")
-    print("# Matches: " + squad_lifetime_stats["matches"]["value"])
-    print("# Wins: " + squad_lifetime_stats["top1"]["value"])
-    print("Win %: " + squad_lifetime_stats["winRatio"]["value"])
-    print("# Top 3s: " + squad_lifetime_stats["top3"]["value"])
-    print("# Top 6s: " + squad_lifetime_stats["top6"]["value"])
-    print("Kills: " + squad_lifetime_stats["kills"]["value"])
-    print("K/D: " + squad_lifetime_stats["kd"]["value"])
-
-def print_solo_current_stats(solo_current_stats):
-    print("\nCURRENT SEASON SOLO STATS")
-    print("# Matches: " + solo_current_stats["matches"]["value"])
-    print("# Wins: " + solo_current_stats["top1"]["value"])
-    print("Win %: " + solo_current_stats["winRatio"]["value"])
-    print("# Top 10s: " + solo_current_stats["top10"]["value"])
-    print("# Top 25s: " + solo_current_stats["top25"]["value"])
-    print("Kills: " + solo_current_stats["kills"]["value"])
-    print("K/D: " + solo_current_stats["kd"]["value"])
-
-def print_duo_current_stats(duo_current_stats):
-    print("\nCURRENT SEASON DUO STATS")
-    print("# Matches: " + duo_current_stats["matches"]["value"])
-    print("# Wins: " + duo_current_stats["top1"]["value"])
-    print("Win %: " + duo_current_stats["winRatio"]["value"])
-    print("# Top 5s: " + duo_current_stats["top5"]["value"])
-    print("# Top 12s: " + duo_current_stats["top12"]["value"])
-    print("Kills: " + duo_current_stats["kills"]["value"])
-    print("K/D: " + duo_current_stats["kd"]["value"])
-
-def print_squad_current_stats(squad_current_stats):
-    print("\nCURRENT SEASON SQUAD STATS")
-    print("# Matches: " + squad_current_stats["matches"]["value"])
-    print("# Wins: " + squad_current_stats["top1"]["value"])
-    print("Win %: " + squad_current_stats["winRatio"]["value"])
-    print("# Top 3s: " + squad_current_stats["top3"]["value"])
-    print("# Top 6s: " + squad_current_stats["top6"]["value"])
-    print("Kills: " + squad_current_stats["kills"]["value"])
-    print("K/D: " + squad_current_stats["kd"]["value"])
+    # sort the group leaderboard
+    group_leaderboards["Lifetime"] = sorted(group_leaderboards["Lifetime"], key=itemgetter('Wins'))
+    print(group_leaderboards)
 
 #program runs here
 if __name__ == '__main__':
@@ -261,20 +185,6 @@ if __name__ == '__main__':
 #   10. Most Wins (Squad)
 #   11. Most Kills (Squad)
 #   12. Highest K/d (Squad)
-
-# Start screen: Enter 1 or more usernames, no spaces, separated by commas
-# loading screen for each username
-# main menu where you can:
-#   add a person to the list
-#   remove a person from the list
-#   view personal leaderboards for a user (input username)
-#   view leaderboards for the group
-# view leaderboards for a user
-#   Select one leaderboard
-#   Select all leaderboards
-# view leaderboards for the group
-#   Select one Leaderboard
-#   Select all Leaderboards
 
 # Some test code for sorting and printing dictionaries
 #print(config.leaderboard["Leaderboards"]["Lifetime"])
